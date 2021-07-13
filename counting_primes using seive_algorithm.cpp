ad58a16c7c884675032e3ced481c841535f9c07d
SirishaH <84061786+SirishaH@users.noreply.github.com>
2021-07-13 16:55:52 +0530

    Update counting_primes using seive_algorithm.cpp

--- counting_primes using seive_algorithm.cpp
+++ counting_primes using seive_algorithm.cpp
@@ -3,18 +3,19 @@
 using namespace std;
 
 int main() {
-	int n,count=0;
-	cin >> n;									
+	int n,count=0;// n=10
+	cin >> n;	
+	//creating vector with 0's
   vector<int>seive(n+1,0);
   for(int i=2;i<n;i++)
   {
-	seive[i]=1;
+	seive[i]=1;// 0  0  1  1  1  1  1  1  1  1
   }
   for(int i=2;i*i<=n;i++)
   {
 	 if(seive[i]==1)
 	 {
-		  for(int j=i*i;j<=n;j+=i)
+		  for(int j=i*i;j<=n;j+=i)  // 0  0  1  1  0  1  0  1  0  0
 	  {
 		  seive[j]=0;
 	  }
